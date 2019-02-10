9ac9ccb00b04b7cb9a8f1e011ed920fa60f4647c
Barry Anthony McNamara III <bamazap@hotmail.com>
2019-02-10 18:36:42 -0500

    publish: Set homepage
    generated from commit ddf01b2e7c96d5230fb1ed5a68534127f9813579

---
+++ precache-manifest.1129c8e9be74736b4b475b448df0b00a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "69c78c2a352598737cb9",
+    "url": "./static/js/main.69c78c2a.chunk.js"
+  },
+  {
+    "revision": "d59661a8222385de2288",
+    "url": "./static/js/1.d59661a8.chunk.js"
+  },
+  {
+    "revision": "69c78c2a352598737cb9",
+    "url": "./static/css/main.790a3cab.chunk.css"
+  },
+  {
+    "revision": "aa4a9c1f1666dc4a609ad184dba58822",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c4a186c2a311b7422d9fc6eea17845e.js"
+  "./precache-manifest.1129c8e9be74736b4b475b448df0b00a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
